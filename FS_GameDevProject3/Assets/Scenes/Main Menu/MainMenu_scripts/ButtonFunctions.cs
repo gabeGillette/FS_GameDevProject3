416ae337631e7dab2ed651262b9f5c3d9968e873
Gabe Gillette <92653198+gabeGillette@users.noreply.github.com>
2024-11-26 14:55:57 -0500

    Added Scenes for level 1,2,3,4 and made debug menu functional.

---
+++ FS_GameDevProject3/Assets/Scenes/Main Menu/MainMenu_scripts/ButtonFunctions.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    /// <summary>
+    /// GoToScene
+    /// Enters a scene at id.
+    /// </summary>
+    /// <param name="id">The scene index</param>
+    public void GoToScene(int id)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(id);
+    }
+}
