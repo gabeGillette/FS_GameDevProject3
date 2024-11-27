d524bd1c68899e83f7dd040254a78713187cbea0
Gabe Gillette <92653198+gabeGillette@users.noreply.github.com>
2024-11-27 11:09:29 -0500

    Added PlayerScript getter to GameManager.cs. Modified function name in Ipickup.cs

--- FS_GameDevProject3/Assets/Global Assets/Global_scripts/GameManager.cs
+++ FS_GameDevProject3/Assets/Global Assets/Global_scripts/GameManager.cs
@@ -12,12 +12,16 @@ public class GameManager : MonoBehaviour
 
     private List<GameObject> _evidenceList;
 
+    private int _evidenceTotal;
+    private int _evidenceCollected;
+
     
 
     /*------------------------------------------ PUBLIC ACCESSORS */
 
     public static GameManager Instance => _instance;
     public GameObject Player => _player;
+    public PlayerController PlayerScript => _player.GetComponent<PlayerController>();
 
     /*--------------------------------------------- PRIVATE METHODS */
 
@@ -30,11 +34,42 @@ public class GameManager : MonoBehaviour
         // find all the evidence interactables
         _evidenceList = new List<GameObject>();
         _evidenceList.AddRange(GameObject.FindGameObjectsWithTag("Evidence"));
+
+        _evidenceCollected = 0;
+        _evidenceTotal = _evidenceList.Count;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    public void CollectEvidence()
     {
-        
+        _evidenceCollected++;
     }
+
+    public void GetPickup(GameObject pickup)
+    {
+        // TODO display ui message
+
+        // just logging for now
+        Debug.Log("Collected " + pickup.name);
+
+        // get component
+        IPickup pickupScript = pickup.GetComponent<IPickup>();
+
+        // fire event
+        if (pickupScript != null)
+        {
+            pickupScript.CollectEvent();
+        }
+        else 
+        {
+            Debug.LogError("Pickup has no script!");
+        }
+
+        // TODO play sound
+
+        // destroy the object
+        Destroy(pickup);
+    }
+
+
 }
