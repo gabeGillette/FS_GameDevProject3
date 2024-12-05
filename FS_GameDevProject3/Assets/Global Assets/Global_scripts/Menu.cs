64da052171235e95d9d8f4a64de6cd92547344d1
Gabriel Gillette <gabedogman@gmail.com>
2024-12-05 07:12:57 -0500

    Did work on menu system, added sound effects and placeholder music.

---
+++ FS_GameDevProject3/Assets/Global Assets/Global_scripts/Menu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+
+/*--------------------------------------- SERIALIZED */
+
+    [SerializeField] GameObject[] _buttons;
+
+    public void DisableButtons()
+    {
+        foreach(GameObject button in _buttons)
+        {
+            button.GetComponent<Button>().interactable = false;
+        }
+    }
+
+    public void EnableButtons()
+    {
+        foreach(GameObject button in _buttons)
+        {
+            button.GetComponent<Button>().interactable = true;
+        }
+    }
+}
---
+++ FS_GameDevProject3/Assets/Global Assets/Global_scripts/MenuManager.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+
+public class MenuManager : MonoBehaviour
+{
+
+
+    [SerializeField] GameObject _confirmMenu;
+    [SerializeField] GameObject _pauseMenu;
+    [SerializeField] GameObject _mainMenu;
+    [SerializeField] GameObject[] _allMenus;
+
+    [SerializeField] Button _confirmYes;
+    [SerializeField] Button _confirmNo;
+
+    
+
+    public enum MENU {PAUSE, SAVE, LOAD, MAIN, LEVEL_SELECT}
+    public enum BUTTON_FUNCTION {RESUME, QUIT, SAVE, LOAD, NEW_GAME, OPTIONS}
+
+    [SerializeField] GameObject _defaultMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(_defaultMenu != null)
+        {
+            _defaultMenu.gameObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void ConfirmDestructive(UnityAction YesAction, UnityAction NoAction)
+    {
+        DisableAllMenus();
+        _confirmMenu.gameObject.SetActive(true);
+        EnableMenu(_confirmMenu);
+
+        _confirmYes.onClick.AddListener(YesAction);
+        _confirmNo.onClick.AddListener(NoAction);
+
+    }
+
+
+
+
+    public void KillAllMenus()
+    {
+        foreach(GameObject menu in _allMenus)
+        {
+            menu.gameObject.SetActive(false);
+        }
+    }
+
+    public void DisableMenu(GameObject menu)
+    {
+        menu.gameObject.GetComponent<Menu>().DisableButtons();
+    }
+
+    public void EnableMenu(GameObject menu)
+    {
+        menu.gameObject.GetComponent<Menu>().EnableButtons();
+    }
+
+    public void DisableAllMenus()
+    {
+        foreach(GameObject menu in _allMenus)
+        {
+            DisableMenu(menu);
+        }
+    }
+
+
+    public void ButtonFunction(BUTTON_FUNCTION function)
+    {
+        switch(function)
+        {
+            case BUTTON_FUNCTION.RESUME:
+                KillAllMenus();
+                GameManager.Instance.UnpauseGame();
+                break;
+
+            case BUTTON_FUNCTION.QUIT:
+                DisableAllMenus();
+
+
+                break;
+            case BUTTON_FUNCTION.SAVE:
+                break;
+            case BUTTON_FUNCTION.LOAD:
+                break;
+            case BUTTON_FUNCTION.NEW_GAME:
+                break;
+            case BUTTON_FUNCTION.OPTIONS:
+                break;
+            
+        }
+    }
+
+
+}
