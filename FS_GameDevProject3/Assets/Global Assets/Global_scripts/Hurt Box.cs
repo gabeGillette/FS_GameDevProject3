cbf47d8bb6870b70e5ab67b69a8d54d633d71fea
Gabriel Gillette <gabedogman@gmail.com>
2024-12-08 12:11:12 -0500

    worked on level blockout

---
+++ FS_GameDevProject3/Assets/Global Assets/Global_scripts/Hurt Box.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HurtBox : MonoBehaviour
+{
+    [SerializeField] int _dmg_amt;
+    void OnTriggerStay(Collider other)
+    {
+        if(other.GetComponent<IDamage>() != null)
+        {
+            other.GetComponent<IDamage>().takeDamage(_dmg_amt);
+        }
+    }
+}
